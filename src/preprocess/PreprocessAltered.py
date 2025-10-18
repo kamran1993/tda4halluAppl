491fa625368d077962565ef29406a9eac28f6a93
kamran1993 <kamran.alizadeh.rad@hhu.de>
2025-10-18 12:57:55 +0200

    Stratify altered dataset in preprocess in order to have a more realistic partition of hallucinations

--- src/preprocess/PreprocessAltered.py
+++ src/preprocess/PreprocessAltered.py
@@ -17,12 +17,22 @@ class Altered(HallucinationDetectionDataset):
     source_file: str = "data/raw/altered/altered.jsonl"
     split: str = "original"
     val_size: int | float = 100
-    sample_size: int | float = 1000
     random_state: int = 42
+    sample_size: int | float = 1000
+    part_hallu: float = 0.3
+
+    def sample_and_stratify(self, df: pd.DataFrame) -> pd.DataFrame:
+        # the whole dataset is too big and has too many hallucinated samples, therefore we sample it down and stratify it
+        df = df.groupby("hallucination").sample(n=self.sample_size, random_state=self.random_state)
+        n_hallu_samples = round(self.part_hallu * self.sample_size)
+        n_grounded_samples = self.sample_size - n_hallu_samples
+        df = pd.concat([df.iloc[0:n_grounded_samples,:], df.iloc[self.sample_size:(self.sample_size+n_hallu_samples),:]])
+        df = df.sample(frac=1, random_state=self.random_state) # shuffle
+        return df
 
     def split_data(self, df: pd.DataFrame) -> tuple[np.ndarray[int], np.ndarray[int]]:
         """Split."""
-        indices = np.arange(len(df))  # Create an array of integer indices
+        indices = np.arange(len(df))
         if self.val_size == len(indices):
             return None, indices
         train_test_indices, val_indices = train_test_split(
@@ -37,8 +47,6 @@ class Altered(HallucinationDetectionDataset):
 
     def process(self) -> tuple[pd.DataFrame, pd.Series, ndarray, ndarray | None]:
         df = self.load_data()
-        # use smaller sample since the whole dataset is too big
-        df = df.sample(self.sample_size, random_state=self.random_state)
 
         prompt_for_nlg = "Create one response in natural language " \
                          "from this dialogue act. Create nothing else. "
@@ -58,6 +66,8 @@ class Altered(HallucinationDetectionDataset):
         df["id"] = df.index
         # Non-empty alteration_meta means that the utterance is "hallucinated"
         df["hallucination"] = df["hallucination"] != {'field_drops': [], 'injected_noise': [], 'fallback_kept': []}
+        df = self.sample_and_stratify(df)
+
         if self.model_name =="Mistral-7B-Instruct-v0.1":
             df["prompt"] = df.apply(insert_context_question_mistral, axis=1)
             df["response"] = df["response"].apply(lambda x: f"{x}</s>")
