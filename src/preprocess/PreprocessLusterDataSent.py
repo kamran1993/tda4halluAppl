cf2e3e45944a23f88fe05e8866c83f680897d430
kamran1993 <kamran.alizadeh.rad@hhu.de>
2025-11-20 08:19:14 +0100

    Add support for new labeling script for LusterData as well as new results

--- src/preprocess/PreprocessLusterDataSent.py
+++ src/preprocess/PreprocessLusterDataSent.py
@@ -2,12 +2,19 @@ from dataclasses import dataclass
 
 import typing as tp
 import sys
+import os
 import numpy as np
 import pandas as pd
 from numpy import ndarray
+import pickle
+import pathlib
 from sklearn.model_selection import train_test_split
-sys.path.append('/gpfs/project/kaali100/models/LUSTER')
-from luster.process_training_logs import load_create_dataframe
+
+sys.path.append(os.environ.get('LUSTER_REPOSITORY_BASE_PATH'))
+from luster.process_training_logs.prepare_imports_for_pickle_loading import prepare_imports_for_pickle_loading
+from luster.memory import PolicyHistoryItem
+sys.path.append(os.environ.get('CONVLAB3_REPOSITORY_BASE_PATH'))
+from convlab.nlg import load_and_label_data
 
 from .dataset_abc import HallucinationDetectionDataset
 
@@ -31,17 +38,51 @@ class LusterDataSent(HallucinationDetectionDataset):
         )
         return train_test_indices, val_indices
 
+
+    def load_data(self) -> pd.DataFrame:
+        """Load policy history items and convert them to dataframe."""
+        prepare_imports_for_pickle_loading()
+
+        turns_path = pathlib.Path(
+            os.environ['LUSTER_REPOSITORY_BASE_PATH'],
+            "data",
+            "training_logs",
+            "experiences_succ+sent",
+            "analysis_outputs",
+            "turns.pkl",
+        )
+
+        with turns_path.open(mode="rb") as f:
+            policy_histories_list = pickle.load(file=f)
+
+        dataset_name: str = "multiwoz21"
+        df = pd.DataFrame(columns=["id", "prompt", "response", "name"])
+        ind = 0
+        for turn in policy_histories_list:
+            ind += 1
+            utterance = turn.system_response
+            prompt = turn.full_seq_with_top_1_response
+            utterance = ' system : ' + utterance + '</s>'
+            prompt = prompt.removesuffix(utterance)
+            df.loc[ind - 1] = [ind, prompt, utterance, dataset_name]
+
+        return df
+
+
     def process(self) -> tuple[pd.DataFrame, pd.Series, ndarray, ndarray | None]:
         if self.model_name != "LUSTER":
             raise NotImplementedError(
                 f"This model is not supported yet: {self.model_name}"
             )
-        df = load_create_dataframe.createMultiwoz21DataFrame(
-            load_create_dataframe.load_training_logs("experiences_succ+sent"))
+        df = self.load_data()
+        labels = load_and_label_data.load_data_and_create_hallu_labels(
+            os.environ['LUSTER_REPOSITORY_BASE_PATH']
+            + "/data/training_logs/experiences_succ+sent/analysis_outputs/converted_luster_log.json"
+        )
         train_indices, test_indices = self.split_data(df)
         return (
             pd.DataFrame(df[["id", "prompt", "response", "name"]]),
-            df["hallucination"].astype(int),
+            labels.astype(int),
             train_indices,
             test_indices,
         )
