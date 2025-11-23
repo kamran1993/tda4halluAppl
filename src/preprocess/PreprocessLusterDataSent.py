f82ffdf6b794f5f04e3e5a049aced7de77c3a6ee
kamran1993 <kamran.alizadeh.rad@hhu.de>
2025-11-23 10:53:25 +0100

    Reduce code duplication and enable using datasets from other directories

--- src/preprocess/PreprocessLusterDataSent.py
+++ src/preprocess/PreprocessLusterDataSent.py
@@ -1,87 +1,11 @@
-from dataclasses import dataclass
-
-import typing as tp
-import sys
+from dataclasses import dataclass, field
 import os
-import numpy as np
-import pandas as pd
-from numpy import ndarray
-import pickle
-import pathlib
-from sklearn.model_selection import train_test_split
 
-sys.path.append(os.environ.get('LUSTER_REPOSITORY_BASE_PATH'))
-from luster.process_training_logs.prepare_imports_for_pickle_loading import prepare_imports_for_pickle_loading
-from luster.memory import PolicyHistoryItem
-from src.preprocess import labelLusterData
-
-from .dataset_abc import HallucinationDetectionDataset
-
+from .preprocessLuster import PreprocessLuster
 
 @dataclass
-class LusterDataSent(HallucinationDetectionDataset):
+class LusterDataSent(PreprocessLuster):
     """A class to process and manage one of the two luster datasets."""
 
-    model_name: tp.Literal["LUSTER"]
-    split: str = "original"
-    val_size: int | float = 100
-    random_state: int = 42
-
-    def split_data(self, df: pd.DataFrame) -> tuple[np.ndarray[int], np.ndarray[int]]:
-        """Split."""
-        indices = np.arange(len(df))
-        if self.val_size == len(indices):
-            return None, indices
-        train_test_indices, val_indices = train_test_split(
-            indices, test_size=self.val_size, random_state=self.random_state
-        )
-        return train_test_indices, val_indices
-
-
-    def load_data(self) -> pd.DataFrame:
-        """Load policy history items and convert them to dataframe."""
-        prepare_imports_for_pickle_loading()
-
-        turns_path = pathlib.Path(
-            os.environ['LUSTER_REPOSITORY_BASE_PATH'],
-            "data",
-            "training_logs",
-            "experiences_succ+sent",
-            "analysis_outputs",
-            "turns.pkl",
-        )
-
-        with turns_path.open(mode="rb") as f:
-            policy_histories_list = pickle.load(file=f)
-
-        dataset_name: str = "multiwoz21"
-        df = pd.DataFrame(columns=["id", "prompt", "response", "name"])
-        ind = 0
-        for turn in policy_histories_list:
-            ind += 1
-            utterance = turn.system_response
-            prompt = turn.full_seq_with_top_1_response
-            utterance = ' system : ' + utterance + '</s>'
-            prompt = prompt.removesuffix(utterance)
-            df.loc[ind - 1] = [ind, prompt, utterance, dataset_name]
-
-        return df
-
-
-    def process(self) -> tuple[pd.DataFrame, pd.Series, ndarray, ndarray | None]:
-        if self.model_name != "LUSTER":
-            raise NotImplementedError(
-                f"This model is not supported yet: {self.model_name}"
-            )
-        df = self.load_data()
-        labels = labelLusterData.load_data_and_create_hallu_labels(
-            os.environ['LUSTER_REPOSITORY_BASE_PATH']
-            + "/data/training_logs/experiences_succ+sent/analysis_outputs/converted_luster_log.json"
-        )
-        train_indices, test_indices = self.split_data(df)
-        return (
-            pd.DataFrame(df[["id", "prompt", "response", "name"]]),
-            labels.astype(int),
-            train_indices,
-            test_indices,
-        )
+    turns_dir_path: str = field(default_factory=lambda: os.environ.get('LUSTER_REPOSITORY_BASE_PATH')\
+                     + "/data/training_logs/experiences_succ+sent/analysis_outputs")
---
+++ src/preprocess/preprocessLuster.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass
+
+import typing as tp
+import sys
+import os
+import numpy as np
+import pandas as pd
+from numpy import ndarray
+import pickle
+import pathlib
+from sklearn.model_selection import train_test_split
+
+sys.path.append(os.environ.get('LUSTER_REPOSITORY_BASE_PATH'))
+from luster.process_training_logs.prepare_imports_for_pickle_loading import prepare_imports_for_pickle_loading
+from luster.memory import PolicyHistoryItem
+from luster.evaluation.convert_luster_pkl_to_unified import main as convertLusterPklToJson
+from src.preprocess import labelLusterData
+
+from .dataset_abc import HallucinationDetectionDataset
+
+
+@dataclass
+class PreprocessLuster(HallucinationDetectionDataset):
+    """A class to process LUSTER pkl-datasets."""
+
+    model_name: tp.Literal["LUSTER"]
+    turns_dir_path: str
+    split: str = "original"
+    val_size: int | float = 100
+    random_state: int = 42
+
+    def split_data(self, df: pd.DataFrame) -> tuple[np.ndarray[int], np.ndarray[int]]:
+        """Split."""
+        indices = np.arange(len(df))
+        if self.val_size == len(indices):
+            return None, indices
+        train_test_indices, val_indices = train_test_split(
+            indices, test_size=self.val_size, random_state=self.random_state
+        )
+        return train_test_indices, val_indices
+
+
+    def load_data(self) -> pd.DataFrame:
+        """Load policy history items and convert them to dataframe."""
+        prepare_imports_for_pickle_loading()
+
+        turns_path = pathlib.Path(self.turns_dir_path, "turns.pkl")
+
+        with turns_path.open(mode="rb") as f:
+            policy_histories_list = pickle.load(file=f)
+
+        dataset_name: str = "multiwoz21"
+        df = pd.DataFrame(columns=["id", "prompt", "response", "name"])
+        ind = 0
+        for turn in policy_histories_list:
+            ind += 1
+            utterance = turn.system_response
+            prompt = turn.full_seq_with_top_1_response
+            utterance = ' system : ' + utterance + '</s>'
+            prompt = prompt.removesuffix(utterance)
+            df.loc[ind - 1] = [ind, prompt, utterance, dataset_name]
+
+        return df
+
+
+    def process(self) -> tuple[pd.DataFrame, pd.Series, ndarray, ndarray | None]:
+        if self.model_name != "LUSTER":
+            raise NotImplementedError(
+                f"This model is not supported yet: {self.model_name}"
+            )
+        df = self.load_data()
+        converted_luster_log_path = self.turns_dir_path + "/converted_luster_log.json"
+        if not os.path.isfile(converted_luster_log_path):
+            convertLusterPklToJson(pathlib.Path(self.turns_dir_path + "/turns.pkl"))
+        labels = labelLusterData.load_data_and_create_hallu_labels(converted_luster_log_path)
+        train_indices, test_indices = self.split_data(df)
+        return (
+            pd.DataFrame(df[["id", "prompt", "response", "name"]]),
+            labels.astype(int),
+            train_indices,
+            test_indices,
+        )
